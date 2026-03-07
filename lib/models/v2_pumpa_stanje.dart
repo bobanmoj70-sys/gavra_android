d3b205b916ad4801a3669106c866299948d886a6
Lakisa-code <lazargavrilovic72@gmail.com>
2026-03-07 23:33:34 +0100

    fix: v2_pumpa_stanje - static const defaults, runtimeType u ==, copyWith, toString

--- lib/models/v2_pumpa_stanje.dart
+++ lib/models/v2_pumpa_stanje.dart
@@ -1,5 +1,8 @@
 /// Model za trenutno stanje kućne pumpe goriva (v2_pumpa_stanje VIEW)
 class V2PumpaStanje {
+  static const double defaultKapacitet = 3000;
+  static const double defaultAlarmNivo = 500;
+
   final double kapacitetLitri;
   final double alarmNivo;
   final double pocetnoStanje;
@@ -19,8 +22,8 @@ class V2PumpaStanje {
   });
 
   factory V2PumpaStanje.fromJson(Map<String, dynamic> j) => V2PumpaStanje(
-        kapacitetLitri: (j['kapacitet_litri'] as num?)?.toDouble() ?? 3000,
-        alarmNivo: (j['alarm_nivo'] as num?)?.toDouble() ?? 500,
+        kapacitetLitri: (j['kapacitet_litri'] as num?)?.toDouble() ?? defaultKapacitet,
+        alarmNivo: (j['alarm_nivo'] as num?)?.toDouble() ?? defaultAlarmNivo,
         pocetnoStanje: (j['pocetno_stanje'] as num?)?.toDouble() ?? 0,
         ukupnoPunjeno: (j['ukupno_punjeno'] as num?)?.toDouble() ?? 0,
         ukupnoUtroseno: (j['ukupno_utroseno'] as num?)?.toDouble() ?? 0,
@@ -34,14 +37,15 @@ class V2PumpaStanje {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is V2PumpaStanje &&
+      (other is V2PumpaStanje &&
+          runtimeType == other.runtimeType &&
           kapacitetLitri == other.kapacitetLitri &&
           alarmNivo == other.alarmNivo &&
           pocetnoStanje == other.pocetnoStanje &&
           ukupnoPunjeno == other.ukupnoPunjeno &&
           ukupnoUtroseno == other.ukupnoUtroseno &&
           trenutnoStanje == other.trenutnoStanje &&
-          procenatPune == other.procenatPune;
+          procenatPune == other.procenatPune);
 
   @override
   int get hashCode => Object.hash(
@@ -53,4 +57,28 @@ class V2PumpaStanje {
         trenutnoStanje,
         procenatPune,
       );
+
+  V2PumpaStanje copyWith({
+    double? kapacitetLitri,
+    double? alarmNivo,
+    double? pocetnoStanje,
+    double? ukupnoPunjeno,
+    double? ukupnoUtroseno,
+    double? trenutnoStanje,
+    double? procenatPune,
+  }) {
+    return V2PumpaStanje(
+      kapacitetLitri: kapacitetLitri ?? this.kapacitetLitri,
+      alarmNivo: alarmNivo ?? this.alarmNivo,
+      pocetnoStanje: pocetnoStanje ?? this.pocetnoStanje,
+      ukupnoPunjeno: ukupnoPunjeno ?? this.ukupnoPunjeno,
+      ukupnoUtroseno: ukupnoUtroseno ?? this.ukupnoUtroseno,
+      trenutnoStanje: trenutnoStanje ?? this.trenutnoStanje,
+      procenatPune: procenatPune ?? this.procenatPune,
+    );
+  }
+
+  @override
+  String toString() =>
+      'V2PumpaStanje(trenutno: $trenutnoStanje L, kapacitet: $kapacitetLitri L, procenat: $procenatPune%)';
 }
