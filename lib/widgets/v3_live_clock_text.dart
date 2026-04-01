0b59380011f314c482ffc47fe7eea9bdd65dba3b
Lakisa-code <lazargavrilovic72@gmail.com>
2026-04-01 17:51:40 +0200

    Refactor live clock to info-only local timer widget

--- lib/widgets/v3_live_clock_text.dart
+++ lib/widgets/v3_live_clock_text.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-class V3LiveClockText extends StatelessWidget {
+class V3LiveClockText extends StatefulWidget {
   final TextStyle? style;
   final TextAlign textAlign;
   final int precisionSeconds;
@@ -14,29 +14,61 @@ class V3LiveClockText extends StatelessWidget {
     this.precisionSeconds = 1,
   }) : assert(precisionSeconds > 0);
 
+  @override
+  State<V3LiveClockText> createState() => _V3LiveClockTextState();
+}
+
+class _V3LiveClockTextState extends State<V3LiveClockText> {
+  Timer? _timer;
+  late DateTime _now;
+
+  @override
+  void initState() {
+    super.initState();
+    _now = DateTime.now();
+    _startTimer();
+  }
+
+  @override
+  void didUpdateWidget(covariant V3LiveClockText oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.precisionSeconds != widget.precisionSeconds) {
+      _startTimer();
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer?.cancel();
+    _timer = Timer.periodic(Duration(seconds: widget.precisionSeconds), (_) {
+      if (!mounted) return;
+      setState(() {
+        _now = DateTime.now();
+      });
+    });
+  }
+
   String _formatNow() {
-    final now = DateTime.now();
-    final h = now.hour.toString().padLeft(2, '0');
-    final m = now.minute.toString().padLeft(2, '0');
-    final s = now.second.toString().padLeft(2, '0');
+    final h = _now.hour.toString().padLeft(2, '0');
+    final m = _now.minute.toString().padLeft(2, '0');
+    final s = _now.second.toString().padLeft(2, '0');
     return '$h:$m:$s';
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<int>(
-      stream: Stream<int>.periodic(Duration(seconds: precisionSeconds), (tick) => tick),
-      initialData: 0,
-      builder: (context, _) {
-        return Text(
-          _formatNow(),
-          style: style,
-          textAlign: textAlign,
-          maxLines: 1,
-          softWrap: false,
-          overflow: TextOverflow.ellipsis,
-        );
-      },
+    return Text(
+      _formatNow(),
+      style: widget.style,
+      textAlign: widget.textAlign,
+      maxLines: 1,
+      softWrap: false,
+      overflow: TextOverflow.ellipsis,
     );
   }
 }
