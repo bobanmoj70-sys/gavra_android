297e345325638aa9f90172f1f7282fb39d007327
Lakisa-code <lazargavrilovic72@gmail.com>
2026-04-11 07:25:49 +0200

    fix: force play integrity provider for Firebase App Check

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart' as fcm;
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -155,7 +154,7 @@ Future<void> _initFirebaseSync() async {
 
       try {
         await FirebaseAppCheck.instance.activate(
-          androidProvider: kDebugMode ? AndroidProvider.debug : AndroidProvider.playIntegrity,
+          androidProvider: AndroidProvider.playIntegrity,
           appleProvider: AppleProvider.appAttest,
         );
         debugPrint('✅ [Firebase] App Check aktiviran');
