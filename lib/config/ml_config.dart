202410905711206a44903713e0008917afabed1e
Lakisa-code <lazargavrilovic72@gmail.com>
2026-06-14 14:20:45 +0200

    Promenjen ML config na HTTP umesto HTTPS
    - SSL handshake greška sa Tailscale URL-om
    - HTTP je dovoljan jer je Tailscale već bezbedan tunnel
    - Dodat port 8000 u URL

--- lib/config/ml_config.dart
+++ lib/config/ml_config.dart
@@ -1,4 +1,4 @@
 class MlConfig {
-  // Tailscale Funnel - javna HTTPS adresa, radi bez Tailscale app na telefonu
-  static const baseUrl = 'https://win-vfeglqf71ss.tail61b7a2.ts.net';
+  // Tailscale Funnel - HTTP je dovoljan jer je Tailscale već bezbedan tunnel
+  static const baseUrl = 'http://win-vfeglqf71ss.tail61b7a2.ts.net:8000';
 }
